91af682ac53078710f9567727508b4177eb2d83e
Andy Marks <amarks@thoughtworks.com>
2017-05-17 10:17:11 +1000

    First version of install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+set -e
+
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Update dotfiles itself first
+
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# Bunch of symlinks
+
+ln -sfv "$DOTFILES_DIR/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/.bashrc" ~
+ln -sfv "$DOTFILES_DIR/.bash-powerline.sh" ~
+ln -sfv "$DOTFILES_DIR/.bash_aliases" ~
+ln -sfv "$DOTFILES_DIR/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/.taskrc" ~
+ln -sfv "$DOTFILES_DIR/.vimrc" ~
+ln -sfv "$DOTFILES_DIR/profiles.clj" ~/.lein/
+
