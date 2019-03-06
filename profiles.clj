bfe673fb780887eb4d23915783b6e9a5b54fbf82
Andy Marks <vampwillow@gmail.com>
2019-03-06 19:26:57 +1100

    Newer version of test-refresh lib

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,7 @@
 {:user 
   {:plugins 
     [
-      [lein-ancient "0.6.10"]
+      [lein-ancient "0.6.15"]
       [venantius/ultra "0.5.0"]
       [jonase/eastwood "0.2.4"]
       [lein-kibit "0.1.5"]
@@ -9,7 +9,7 @@
       [lein-release "1.0.9"]
       [com.chartbeat.cljbeat/lein-assemble "0.1.4"]
       [lein-midje "3.2.1"]
-      [com.jakemccrary/lein-test-refresh "0.19.0"]]
+      [com.jakemccrary/lein-test-refresh "0.23.0"]]
   :dependencies
     [[slamhound "1.5.5"]]
   :aliases 
